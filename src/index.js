1504db5b84d17b3069bbf7a5a26097433a1b4b37
DNIWE08 <olimp12345678@gmail.com>
2021-03-14 16:28:40 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let cfg = bracketsConfig.map(el => {
+    return el.join("");
+  })
+
+  for (let j = 0; j < cfg.length; j++) {
+    if (str.includes(cfg[j])) {
+      str = str.replace(cfg[j], '')
+      j = -1;
+    }
+  }
+  return str === '';
 }
